8de2be6ba4bfe507dfe38984647c7a72ef0d6eab
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-22 16:36:56 +0200

    Add proper test

--- test/worker_test.js
+++ test/worker_test.js
@@ -7,67 +7,36 @@ const sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
 
-const Worker = require('../worker.js');
-const WebApp = require('../webapp.js');
+const bundlerInit = require('../worker.js');
 
-describe('template worker test example', function () {
-  let work;
-  let io;
-  let context;
-  let config;
-  let job;
-  let exitCode;
+describe('bundler', () => {
+  let bundler;
 
-  function prepareWorker(done) {
-    context = {};
-    io = {
-      on: sinon.stub().yields('123', {exitCode: exitCode}),
-      removeListener: sinon.stub(),
-      emit: sinon.stub()
+  beforeEach(() => {
+    const config = {
+      bundleDirectory: 'dist'
     };
-
-    Worker.init(config, job, sinon.stub(), function (err, res) {
-      work = function () {
-        return res;
-      };
-
-      if (done) {
-        done();
-      }
-    });
-  }
-
-  beforeEach(function (done) {
-    exitCode = 0;
-    job = {
-      project: {name: 'strider-bundler'},
-      ref: {branch: 'master'},
-      _id: '123',
-      trigger: {
-        type: 'manual'
-      }
-    };
-    process.env.strider_server_name = 'http://example.com';
-    config = {
-      environment: 'test message',
-      prepare: 'test message'
-    };
-    //_.each(schema, function(v,k) { config[k] = v.default });
-    //config.token = 'token';
-    //config.subdomain = 'subdomain';
-
-    prepareWorker(done);
+    const job = {};
+    return bundlerInit.initAsync(config, job)
+      .then(result => bundler = result);
   });
 
-  it('should set up the phase actions properly', function () {
-    const setup = work();
-    //environment returns a string
-    expect(setup.environment).to.equal('echo "test message"');
-    //prepare returns an object
-    expect(setup.prepare.command).to.equal('echo');
-    expect(setup.prepare.args[0]).to.equal('"test message"');
+  it('should run tar', () => {
+    let contextCmd = sinon.stub();
+    contextCmd.onFirstCall().callsArg(1);
 
-    //test returns a function
-    expect(typeof setup.test).to.equal('function');
+    const context = {
+      comment: Function.prototype,
+      cmd: contextCmd
+    };
+
+    return bundler.deployAsync(context)
+      .then(() => {
+        expect(contextCmd).to.have.been.calledWithMatch({
+          command: 'tar',
+          args: ['--create', '--verbose', '--gzip', '--directory=dist', '--file=package.tgz', '.']
+        });
+      });
+
   });
 });
